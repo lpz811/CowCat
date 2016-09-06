4c4b8d471f26f19fe5fe4e3606c6e4cb85af978a
lpz811 <1243469511@qq.com>
2016-09-07 07:21:05 +0800

    CawCat  first

---
+++ config/repository.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'models'     => [
+        'menu'         => 'App\Models\Menu',
+        'user'         => 'App\Models\User',
+        'user-profile' => 'App\Models\UserProfile',
+        'role'         => 'App\Models\Role',
+        'action'       => 'App\Models\Action',
+        'permission'   => 'App\Models\Permission',
+    ],
+    'page-limit' => 10,
+];
